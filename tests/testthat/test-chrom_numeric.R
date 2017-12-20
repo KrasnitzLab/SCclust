9a6c1f85d8ef10f302b8ca62377b81d9dd9ad181
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2017-12-20 13:14:33 +0200

    new function to convert chom names to numeric added...

---
+++ tests/testthat/test-chrom_numeric.R
@@ -0,0 +1,20 @@
+context("chrom_numeric works properly")
+
+test_that("chrom_numeric works for chr1 and chr2", {
+  chrom <- c("chr1", "chr1", "chr2", "chr2")
+  bin_mat <- data.frame(chrom)
+
+  res <- chrom_numeric(bin_mat)
+  head(res)
+  expect_that(res$chrom.numeric, equals(c(1,1,2,2)))
+})
+
+
+test_that("chrom_numeric works for chrX and chrY", {
+  chrom <- c("chrX", "chrX", "chrY", "chrY")
+  bin_mat <- data.frame(chrom)
+
+  res <- chrom_numeric(bin_mat)
+  head(res)
+  expect_that(res$chrom.numeric, equals(c(23,23,24,24)))
+})
