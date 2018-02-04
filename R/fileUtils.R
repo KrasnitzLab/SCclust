524eaddffe31f87e11ac268369c78293146be857
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2018-02-04 16:34:00 +0200

    #6 preparation of integration test case for the package
    Task-Url: http://github.com/seqpipe/SCclust/issues/6
    nyu011 test case explored...

--- R/fileUtils.R
+++ R/fileUtils.R
@@ -15,3 +15,41 @@ varbin_input_files <- function(input_file_dir, suffix_pattern="") {
   files <- data.frame(names, cells, paths)
   return(files)
 }
+
+
+load_table <- function(filename) {
+  assertthat::assert_that(file.exists(filename))
+
+  df = read.table(filename, header=T, as.is=T)
+  return(df)
+}
+
+uber_cells <- function(df, skip=3) {
+  cell <- colnames(df)[c((skip+1):ncol(df))]
+  return(data.frame(cell))
+}
+
+cells_filename <- function(output_dir, casename) {
+  assertthat::assert_that(file.exists(output_dir))
+
+  filename <- paste(casename, ".cells.txt")
+  outname <- file.path(output_dir, filename)
+  return(outname)
+}
+
+ratio_filename <- function(output_dir, casename) {
+
+}
+
+case_filenames <- function(output_dir, casename) {
+  suffixes <- list(
+    'ratio', 'clone', 'tree', 'seg',
+    'featuremat', 'features',
+    'guide', 'genome', 'cells')
+
+  filenames <- lapply(
+    suffixes,
+    function(s) file.path(output_dir, paste(casename, ".", s, ".txt", sep="")))
+  names(filenames) <- suffixes
+  return(filenames)
+}
