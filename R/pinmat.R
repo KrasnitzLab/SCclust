b311b5a8e145dbaa518d354df8a936200bfee765
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2018-02-19 13:49:22 +0200

    #9 review and implement calculation of pins and pinmat
    Task-Url: http://github.com/seqpipe/SCclust/issues/9

--- R/pinmat.R
+++ R/pinmat.R
@@ -70,24 +70,118 @@ calc_ploidies <- function(gc_df, segment_df, a=NULL) {
   return(ploidies)
 }
 
-filter_segments_short <- function(short_df, eviltwins=NULL, dropareas=NULL) {
+calc_censored_index <- function(short_df, dropareas) {
+  assertthat::assert_that(!is.null(dropareas))
+  censored <-
+    ((short_df[, "chromstart"] >= dropareas[short_df[, "chrom"], "from"]) &
+       (short_df[, "chromend"] <= dropareas[short_df[, "chrom"], "to"]))
+  censoredtoo <-
+    (which(censored) + 1)[(which(censored) + 1) <= nrow(short_df)]
+
+  censored[censoredtoo] <-
+    ((short_df[censoredtoo, "chrom"] == short_df[censoredtoo - 1, "chrom"]) &
+       (short_df[censoredtoo, "profid"] == short_df[censoredtoo - 1, "profid"])) | censored[censoredtoo]
+  return(censored)
+}
+
+filter_dropareas_short <- function(short_df, dropareas = NULL) {
+  if (!is.null(dropareas)) {
+    censored <- calc_censored_index(short_df, dropareas)
+    return(short_df[!censored,])
+  }
+  return(short_df)
+}
+
+filter_evil_short <- function(short_df, eviltwins=NULL) {
   # print(head(short_df))
   good_cells <- unique(short_df[,"profid"])
-  print(good_cells)
-  print(dim(short_df))
   if(!is.null(eviltwins)) {
     good_cells <- setdiff(good_cells, eviltwins)
     short_df <- short_df[short_df[, "profid"] %in% good_cells, ]
   }
-  print(good_cells)
-  print(dim(short_df))
+  return(short_df)
+}
 
-  if(!is.null(dropareas)) {
-    censored <- ((short_df[,"chromstart"]>=dropareas[short_df[,"chrom"],"from"]) &
-                   (short_df[,"chromend"]<=dropareas[short_df[,"chrom"],"to"]))
-    censoredtoo <- (which(censored)+1)[(which(censored)+1)<=nrow(short_df)]
+calc_smear_breakpoints <- function(short_df, censored, smear=1, keepboundaries=F, chromrange=1:22) {
+  dtshort<-cbind(
+    short_df[,c("profid","chrom")],
+    short_df[,"segstarts"]-smear,
+    short_df[,"segstarts"]+smear,
+    sign(short_df[,"cvals"]-c(0,short_df[-nrow(short_df),"cvals"]))
+  )
+  dimnames(dtshort)[[2]]<-c("profid","chrom","bpstart","bpend","bpsign")
 
-    censored[censoredtoo] <- ((short_df[censoredtoo,"chrom"]==short_df[censoredtoo-1,"chrom"])&
-                                (short_df[censoredtoo,"profid"]==short_df[censoredtoo-1,"profid"])) | censored[censoredtoo]
+  ustart<-short_df[match(unique(short_df[,"chrom"]),short_df[,"chrom"]),"segstarts"]
+  uend<-c((ustart-1)[-1],short_df[nrow(short_df),"segends"])
+
+  if(keepboundaries){
+    dtshort[(dtshort[,"bpstart"]+smear)==ustart[dtshort[,"chrom"]],"bpsign"]<-
+      2*dtshort[(dtshort[,"bpstart"]+smear)==ustart[dtshort[,"chrom"]],"bpsign"]
+    dtshort<-dtshort[!censored,]
+  } else {
+    dtshort<-dtshort[((dtshort[,"bpstart"]+smear) > ustart[dtshort[,"chrom"]])&!censored,]
+    dtshort[dtshort[,"bpstart"]<ustart[dtshort[,"chrom"]],"bpstart"]<-
+      ustart[dtshort[dtshort[,"bpstart"]<ustart[dtshort[,"chrom"]],"chrom"]]
+    dtshort[dtshort[,"bpend"]>uend[dtshort[,"chrom"]],"bpend"]<-
+      uend[dtshort[dtshort[,"bpend"]>uend[dtshort[,"chrom"]],"chrom"]]
   }
+  dtshort<-dtshort[dtshort[,"chrom"]%in%chromrange,]
+  return(dtshort)
+}
+
+containment.indicator <- function(vstart, vend, wstart, wend){
+  lw<-length(wstart)
+  lv<-length(vstart)
+  z<-cbind(c(vend,wend),c(1:lv,rep(0,lw)),c(rep(0,lv),1:lw))
+  z<-z[order(z[,1]),]
+  endbeforeend<-cummax(z[,2])[order(z[,3])][sort(z[,3])!=0]
+  z<-cbind(c(wstart,vstart),c(rep((lv+1),lw),1:lv),c(1:lw,rep(0,lv)))
+  z<-z[order(z[,1]),]
+  startafterstart<-rev(cummin(rev(z[,2])))[order(z[,3])][sort(z[,3])!=0]
+  return(cbind(startafterstart,endbeforeend))
+}
+
+calc_pinmat <- function(short_df, smear_df) {
+  allsigns<-sort(unique(smear_df[,"bpsign"]))
+  for(vsign in allsigns){
+    a<-smear_df[smear_df[,"bpsign"]==vsign,]
+    a<-a[order(a[,"bpend"]),]
+    apins<-NULL
+    while(nrow(a)>0){
+      apins<-c(apins,a[1,"bpend"])
+      a<-a[!(a[,"bpstart"]<=apins[length(apins)]&a[,"bpend"]>=apins[length(apins)]),,drop=F]
+    }
+    a<-smear_df[smear_df[,"bpsign"]==vsign,]
+    ci<-containment.indicator(apins,apins,a[order(a[,"bpend"]),"bpstart"],a[order(a[,"bpend"]),"bpend"])
+    a<-cbind(a[order(a[,"bpend"]),],ci)
+
+    dimnames(a)[[2]][(ncol(a)-1):ncol(a)]<-c("startpin","endpin")
+    apinmat<-matrix(ncol=length(unique(short_df[,"profid"])),nrow=length(apins)+2,data=0,
+                    dimnames=list(NULL,unique(short_df[,"profid"])))
+    for(id in unique(a[,"profid"])){
+      apinmat[a[a[,"profid"]==id,"startpin"]+1,id]<-1
+      apinmat[a[a[,"profid"]==id,"endpin"]+2,id]<-apinmat[a[a[,"profid"]==id,"endpin"]+2,id]-1
+    }
+    apinmat<-apply(apinmat,2,cumsum)
+    apinmat<-apinmat[-c(1,nrow(apinmat)),,drop=F]
+    apins<-cbind(apins,rep(vsign,length(apins)))
+    dimnames(apins)[[2]]<-c("bin","sign")
+    if(vsign==min(allsigns)){
+      pinmat<-apinmat
+      pins<-apins
+    }
+    else{
+      pinmat<-rbind(pinmat,apinmat)
+      pins<-rbind(pins,apins)
+    }
+  }
+  pins<-pins[(rowSums(pinmat)<ncol(pinmat)),,drop=F]
+  pinmat<-pinmat[(rowSums(pinmat)<ncol(pinmat)),,drop=F]
+  pinmat_df <- data.frame(pinmat)
+  pins_df <- data.frame(pins)
+
+  res <- list(pinmat_df, pins_df)
+  names(res) <- c("pinmat", "pins")
+
+  return(res)
 }
