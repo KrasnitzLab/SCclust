c3bbed96e1d7b590509afbe31a77fb60b099968b
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2018-03-14 13:59:10 +0200

    #14 find clones review and testing
    Task-Url: http://github.com/seqpipe/SCclust/issues/14

--- run_tests.R
+++ run_tests.R
@@ -3,8 +3,8 @@ library(testthat)
 
 devtools::load_all()
 
-# testthat::test_file("tests/testthat/test_fisher_fdr.R")
-# testthat::test_file("tests/testthat/test_hclust_tree.R")
+testthat::test_file("tests/testthat/test_fisher_fdr.R")
+testthat::test_file("tests/testthat/test_hclust_tree.R")
 testthat::test_file("tests/testthat/test_find_clones.R")
 
 # testthat::test_dir("tests/")
