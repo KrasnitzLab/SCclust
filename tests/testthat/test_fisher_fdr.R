fbd32ea3c294da2b61b934e2b2b0f18958e3bb49
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2018-03-14 10:40:01 +0200

    #12 review and implement fisherFDR
    Task-Url: http://github.com/seqpipe/SCclust/issues/12

--- tests/testthat/test_fisher_fdr.R
+++ tests/testthat/test_fisher_fdr.R
@@ -3,9 +3,12 @@ context("check fisher_fdr works as expected")
 test_that("fisher_fdr works as expected", {
   data_dir <- Sys.getenv("SGAINS_DATA")
 
-  true_filename <- file.path(data_dir, "gleason6.1/results/GL6.1trueP.txt")
-  sim_filename <- file.path(data_dir, "gleason6.1/results/GL6.1simP.txt")
-  pinmat_filename <- file.path(data_dir, "gleason6.1/results/GL6.1smear1bpPinMat.txt")
+  true_filename <- file.path(
+          data_dir, "gleason6.1/results/GL6.1trueP.txt")
+  sim_filename <- file.path(
+          data_dir, "gleason6.1/results/GL6.1simP.txt")
+  pinmat_filename <- file.path(
+          data_dir, "gleason6.1/results/GL6.1smear1bpPinMat.txt")
 
   expect_true(file.exists(true_filename))
   expect_true(file.exists(sim_filename))
@@ -19,12 +22,30 @@ test_that("fisher_fdr works as expected", {
   cellnames <- uber_cells(pinmat, skip=0)
 
 
-  res <- fdr_fisherPV(true_mat, sim_mat, cellnames, graphic=FALSE)
+  res <- fisher_fdr(true_mat, sim_mat, cellnames[,1])
 
   mfdr <- res$mat_fdr
   mdist <- res$mat_dist
 
-  print(dim(mfdr))
-  print(dim(mdist))
+  mdist_filename <- file.path(
+          data_dir,
+          "gleason6.1/results/",
+          "GL6.1smear1bpLog10FisherP.txt")
+  expect_true(file.exists(mdist_filename))
+  mdist_bench <- load_table(mdist_filename)
+  
+  expect_equal(
+          data.matrix(mdist), 
+          data.matrix(mdist_bench), tolerance=1e-6)
+  
+  mfdr_filename <- file.path(
+          data_dir,
+          "gleason6.1/results/",
+          "GL6.1smear1bpLog10FisherFDR.txt")
+  expect_true(file.exists(mfdr_filename))
+  mfdr_bench <- load_table(mfdr_filename)
 
+  expect_equal(
+          data.matrix(mfdr), 
+          data.matrix(mfdr_bench), tolerance=1e-6)
 })
--- tests/testthat/test_simfisher.R
+++ tests/testthat/test_simfisher.R
@@ -3,11 +3,13 @@ context("check simfisher works as expected")
 test_that("simfisher_wrapper works as expected", {
 
   data_dir <- Sys.getenv("SGAINS_DATA")
-  pinmat_filename <- file.path(data_dir, "gleason6.1/results/GL6.1smear1bpPinMat.txt")
+  pinmat_filename <- file.path(
+          data_dir, "gleason6.1/results/GL6.1smear1bpPinMat.txt")
   expect_true(file.exists(pinmat_filename))
   pinmat_df <- load_table(pinmat_filename)
 
-  pins_filename <- file.path(data_dir, "gleason6.1/results/GL6.1smear1bpPins.txt")
+  pins_filename <- file.path(
+          data_dir, "gleason6.1/results/GL6.1smear1bpPins.txt")
   expect_true(file.exists(pins_filename))
   pins_df <- load_table(pins_filename)
 
@@ -17,7 +19,8 @@ test_that("simfisher_wrapper works as expected", {
 
   true_res <- res$true
 
-  true_filename <- file.path(data_dir, "gleason6.1/results/GL6.1trueP.txt")
+  true_filename <- file.path(
+          data_dir, "gleason6.1/results/GL6.1trueP.txt")
   expect_true(file.exists(true_filename))
   true_mat <- as.matrix(scan(true_filename))
 
@@ -27,7 +30,9 @@ test_that("simfisher_wrapper works as expected", {
   # print("sim_res>")
   # print(dim(sim_res))
 
-  sim_filename <- file.path(data_dir, "gleason6.1/test_results/sim_fisher_test_2_of_500_by_200_simP.txt.gz")
+  sim_filename <- file.path(
+          data_dir, 
+          "gleason6.1/test_results/sim_fisher_test_2_of_500_by_200_simP.txt.gz")
   expect_true(file.exists(sim_filename))
   sim_mat <- as.matrix(scan(sim_filename))
   # print("sim_mat>")
