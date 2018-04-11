2576833604a33c393e19e1667a1aca679f49bacd
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2018-04-11 11:27:29 +0300

    #24 check and reproduce subclones results for gl7.2 case
    Task-Url: http://github.com/seqpipe/SCclust/issues/24

---
+++ working/makeCloneTracksNewGL72.R
@@ -0,0 +1,54 @@
+devtools::load_all()
+
+data_dir <- Sys.getenv("SGAINS_DATA")
+assertthat::assert_that(file.exists(data_dir))
+
+case_dir <- file.path(data_dir, "gleason7.2")
+assertthat::assert_that(file.exists(case_dir))
+
+prostate <- "GL7.2"
+
+rdir <- file.path(
+    case_dir,
+    "results2/")
+assertthat::assert_that(file.exists(rdir))
+
+outdir <- file.path(
+    case_dir,
+    "comp2/")
+assertthat::assert_that(file.exists(outdir))
+
+setwd(outdir)
+
+
+clonetype<-"soft"
+subcloneTooBig<-0.8
+hcfile<-system("ls|grep smear1bpLog10FisherHCP.rda",intern=T)
+prostate<-substring(hcfile,first=1,
+	last=nchar(hcfile)-nchar("smear1bpLog10FisherHCP.rda"))
+load(hcfile)
+hc<-get(substring(hcfile,first=1,last=nchar(hcfile)-4))
+rm(list=substring(hcfile,first=1,last=nchar(hcfile)-4))
+clonetable<-data.frame(hc$labels,rep(0,length(hc$labels)),
+	rep(0,length(hc$labels)),stringsAsFactors=F)
+dimnames(clonetable)[[2]]<-c("ID","clone","subclone")
+for(nodes in unique(hc$softclones[clonetype,]))
+	clonetable[hc$leaflist[[nodes]],"clone"]<-nodes
+clonedirs<-system("ls|grep cloneP",intern=T)
+if(length(clonedirs)>0)for(dirs in clonedirs){
+	setwd(dirs)
+	hcfile<-system("ls|grep smear1bpLog10FisherHCP.rda",intern=T)
+	load(hcfile)
+	hc<-get(substring(hcfile,first=1,last=nchar(hcfile)-4))
+	rm(list=substring(hcfile,first=1,last=nchar(hcfile)-4))
+	clunique<-unique(hc$softclones[clonetype,])
+	if(length(clunique)>1)for(nodes in clunique)
+		clonetable[match(hc$labellist[[nodes]],clonetable[,1]),"subclone"]<-nodes
+	if(length(clunique)==1)
+		if(hc$nodesize[clunique]<(subcloneTooBig*max(hc$nodesize)))
+			clonetable[match(hc$labellist[[clunique]],clonetable[,1]),"subclone"]<-
+				clunique
+	setwd("..")
+}
+write.table(clonetable,paste(prostate,"smear1bpFisherPcloneTracks.txt",sep=""),
+	col.names=T,row.names=F,sep="\t",quote=F)
